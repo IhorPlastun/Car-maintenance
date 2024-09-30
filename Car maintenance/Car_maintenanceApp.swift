5989ee1060caf95d4a62af04a0e016e301ea0e1e
Ihor <futureseniordev@outlook.com>
2024-09-30 15:14:37 +0300

    Initial Commit

---
+++ Car maintenance/Car_maintenanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_maintenanceApp.swift
+//  Car maintenance
+//
+//  Created by Igor Plastun on 30.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Car_maintenanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
